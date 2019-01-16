4ba079706bbdd6723fdcb2200f1447f930a99a75
Kiki <kikiwithyou@gmail.com>
2019-01-17 00:00:02 +0800

    Kiki demo with Doctor
    added a few example node to mainScene

--- src/UI/MainScene.java
+++ src/UI/MainScene.java
@@ -2,6 +2,9 @@ package UI;
 
 import Main.Config;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -11,22 +14,45 @@ public class MainScene extends BorderPane {
 
     //All ui element are created as private field
     private Label firstMessage;
+    private Label secondMessage;
+
+    //menu
+    private MenuBar menuBar;
+    private Menu fileMenu;
+    private MenuItem importFile;
+    private MenuItem exportFile;
 
     //constructor is used to constructor all private UI element
     public MainScene(Stage stage){
         firstMessage=new Label("HelloWorld");
+        secondMessage=new Label("Created by Edward as example");
+
+        menuBar=new MenuBar();
+        fileMenu=new Menu("File");
+        importFile=new MenuItem("Import");
+        exportFile=new MenuItem("Export");
+
+
         Connect();
         Style();
     }
 
     //connect UI element to form a DOM tree
     public void Connect(){
-        this.setTop(firstMessage);
+
+        fileMenu.getItems().addAll(importFile,exportFile);
+        menuBar.getMenus().add(fileMenu);
+        this.setTop(menuBar);
+        this.setCenter(firstMessage);
+        this.setBottom(secondMessage);
     }
     //refer to Config and add class to the label
     //if wanted to change style, change Application.css
     public void Style(){
+
         this.getStylesheets().add(Config.CSS_STYLES);
+        firstMessage.getStyleClass().add("haha");
+
     }
 
 
